0a5f9eb542dfee625cf1338351a2cd1c9eb81721
Christian Reyes <cjreyes@andrew.cmu.edu>
2011-11-18 19:27:21 -0500

    fixed logging

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ io.sockets.on('connection', function (socket) {
   
   socket.on('disconnect', function () {    
     delete users[socket.id];
-    log("disconnect" + socket.id);
+    log("disconnect " + socket.id);
     io.sockets.emit('user disconnected', socket.id);
   });
   
