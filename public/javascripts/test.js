3550692bdb77064dede25c3bc8601ebf226d514d
Christian Reyes <cjreyes@andrew.cmu.edu>
2011-11-21 01:27:33 -0500

    almost ready for integration with server

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -6,21 +6,16 @@ $(function(){
   
   for(var r=0; r<ROWS ; r++){
     MAZE[r] = {};
-  }
-  
-  var paper  = new Raphael($("#canvas_container")[0], 500, 500);
-    
-  var circle = paper.circle(30,30, 10).attr({fill: "red", stroke:"none"});
-  
-  circle.row = 1;
-  circle.col = 1;
-  
-
+  } // initialize every row as an empty object
   
   var STEP = 20;
   
   var WALL_WIDTH = 20;
   
+  var paper  = new Raphael($("#canvas_container")[0], 500, 500);
+    
+  var my_circle = make_circle(paper, 1, 1, STEP, "red");
+
   drawMaze(MAZE, paper, 25, 25, WALL_WIDTH);
   
   var key_to_dir = { 
@@ -36,29 +31,17 @@ $(function(){
                   /* left */  37: { r:  0, c: -1 }, 
                   /* right */ 39: { r:  0, c:  1 }
                   };
-                  
-  circle.canMove = function(coord_diffs){
-    var newR = this.row + coord_diffs.r;
-    var newC = this.col + coord_diffs.c;
-
-    return ! MAZE[newR][newC];
-  }
   
   $(window).bind("keydown", function(e){
     var key = e.keyCode ? e.keyCode : e.which ;
     
     if( key in key_to_dir) {
-      var xy_transform = key_to_dir[ key ];
-      var rc_transform =  key_to_rc[ key ];
+      var rc_diffs =  key_to_rc[ key ];
 
-      if( circle.canMove(rc_transform) ) {
-        circle.row += rc_transform.r;
-        circle.col += rc_transform.c;
-
-        circle.attr({ 
-                      cx: circle.attrs.cx + STEP * xy_transform.x, 
-                      cy: circle.attrs.cy + STEP * xy_transform.y
-                    });
+      if( my_circle.canMove(MAZE, rc_diffs) ) {
+        var xy_diffs = key_to_dir[ key ];
+        
+        my_circle.move(STEP, xy_diffs, rc_diffs);
       }
     }
   });
@@ -72,12 +55,38 @@ function drawMaze(maze, paper, rows, cols, width){
       
       if( border || even ){
         var rect = paper.rect(r * width, c * width, width, width)
-        rect.attr({fill: "green", stroke: "none"});
+        rect.attr({fill: "green", stroke: "none"}); // the wall unit
         
-        maze[r][c] = true;
+        maze[r][c] = true; // true = wall present
       } else {
-        maze[r][c] = false;
+        maze[r][c] = false; // false = wall not present
       } 
     }
   }
+}
+
+function make_circle(paper, row, col, step, color){
+  var circle = paper.circle(row * step + step / 2, col * step + step / 2 , 10).attr({fill: color, stroke:"none"});
+  
+  circle.row = row;
+  circle.col = col;
+  
+  circle.canMove = function(maze, rc_coord_diffs){
+    var newR = this.row + rc_coord_diffs.r;
+    var newC = this.col + rc_coord_diffs.c;
+
+    return ! maze[newR][newC];
+  };
+  
+  circle.move = function(px_step, xy_diffs, rc_diffs){
+    circle.row += rc_diffs.r;
+    circle.col += rc_diffs.c;
+
+    circle.attr({ 
+                  cx: circle.attrs.cx + px_step * xy_diffs.x, 
+                  cy: circle.attrs.cy + px_step * xy_diffs.y
+                });
+  };
+  
+  return circle
 }
